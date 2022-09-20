9464973d027811fff63033db449554a52e8c0480
Elizaveta Stukova <elizaveta.strukowa@yandex.ru>
2022-09-20 15:09:46 +0300

    first commit

---
+++ src/main/java/com/aplicationfx/demo/model/Calculator.java
@@ -0,0 +1,31 @@
+package com.aplicationfx.demo.model;
+
+public class Calculator {
+    private Double num;
+
+    private Double percent;
+    private Double res;
+
+
+    public Double getNum() {
+        return num;
+    }
+
+    public void setNum(Double num) {
+        this.num = num;
+    }
+
+    public Double getPercent() {
+        return percent;
+    }
+
+    public void setPercent(Double percent) {
+        this.percent = percent;
+    }
+
+    public Double calc() {
+        res = (num / 100) * percent ;
+
+        return res;
+    }
+}
